cd39b7c96807a81d8e6ad1d175ce7214456f2bc7
lisnote <didongxiaoli@gmail.com>
2023-05-08 16:40:10 +0800

    简化代码

--- src/views/DataSource/DataSource.vue
+++ src/views/DataSource/DataSource.vue
@@ -1,36 +1,23 @@
 <script setup lang="ts">
-import { guests, dishes, beverages, ingredients } from '@/data';
+import data from '@/data';
+import { ref } from 'vue';
 import common from '@/styles/common.module.scss';
 
-function stringify(value: any) {
-  return JSON.stringify(value, null, 2);
-}
-
-const tabData = [
-  { label: '顾客', value: stringify(guests) },
-  { label: '料理', value: stringify(dishes) },
-  { label: '酒水', value: stringify(beverages) },
-  { label: '食材', value: stringify(ingredients) },
-];
+const activedTab = ref<keyof typeof data>('guests');
 </script>
 
 <template>
-  <div :class="common.contentArea">
-    <el-tabs>
-      <el-tab-pane
-        v-for="item in tabData"
-        :key="item.label"
-        :label="item.label"
-      >
-        <pre class="whitespace-pre-wrap">{{ item.value }}</pre>
-      </el-tab-pane>
-    </el-tabs>
+  <div :class="common.contentArea" class="flex flex-col">
+    <ElTabs v-model="activedTab">
+      <ElTabPane label="顾客" name="guests" />
+      <ElTabPane label="料理" name="dishes" />
+      <ElTabPane label="酒水" name="beverages" />
+      <ElTabPane label="食材" name="ingredients" />
+    </ElTabs>
+    <ElScrollbar>
+      <pre class="flex-1 overflow-auto">{{
+        JSON.stringify(data[activedTab], null, 2)
+      }}</pre>
+    </ElScrollbar>
   </div>
 </template>
-<style scoped lang="scss">
-// pre {
-//   overflow-wrap: break-word;
-//   word-break: break-all;
-//   white-space: break-spaces;
-// }
-</style>
