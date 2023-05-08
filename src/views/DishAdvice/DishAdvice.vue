6f7530e3b44476e9d16962244b2dac0a0cd56b22
lisnote <didongxiaoli@gmail.com>
2023-05-09 01:14:56 +0800

    样式优化

--- src/views/DishAdvice/DishAdvice.vue
+++ src/views/DishAdvice/DishAdvice.vue
@@ -54,7 +54,7 @@ function dishSelect(value: Dish) {
 </script>
 <template>
   <div :class="common.contentArea" class="divide-x-1 flex">
-    <ElScrollbar class="w-200px">
+    <ElScrollbar class="w-200px min-w-200px">
       <GuestSelect @guestSelect="guestSelect($event)" />
     </ElScrollbar>
     <div class="flex-1 pl-20px flex flex-col">
