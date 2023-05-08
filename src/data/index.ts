cbeafb5a2d5c5e346e0b976a54b7a6440ee6d1ac
lisnote <didongxiaoli@gmail.com>
2023-05-08 19:07:28 +0800

    增加常见地址

--- src/data/index.ts
+++ src/data/index.ts
@@ -2,12 +2,14 @@ import beverages from './beverages';
 import dishes from './dishes';
 import ingredients from './ingredients';
 import guests from './guests';
+import location from './location';
 
 const data = {
   beverages,
   dishes,
   guests,
   ingredients,
+  location,
 };
 
 export { data as default, beverages, dishes, guests, ingredients };
