01932937b138adcd3a86844923ab70574d4ea7a4
lisnote <didongxiaoli@gmail.com>
2023-05-08 18:43:54 +0800

    增加工具类型

---
+++ types/utils.d.ts
@@ -0,0 +1,5 @@
+import { DeepReadonly as VueDeepReadonly } from 'vue';
+declare global {
+  type DeepReadonly<T> = VueDeepReadonly<T>;
+  type LooseAutocomplete<T extends string> = T | Omit<string, T>;
+}
