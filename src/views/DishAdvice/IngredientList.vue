228f9f670fa82911dff22a7f31d5d333e7739590
lisnote <didongxiaoli@gmail.com>
2023-05-09 01:11:05 +0800

    完成食材推荐

--- src/views/DishAdvice/IngredientList.vue
+++ src/views/DishAdvice/IngredientList.vue
@@ -1,5 +1,5 @@
 <script setup lang="ts">
-import { Ingredients } from '@/data';
+import type { Ingredients } from '@/data';
 const emit = defineEmits(['update:modelValue']);
 const { ingredient, modelValue } = defineProps<{
   modelValue: Boolean;
@@ -11,7 +11,20 @@ console.log(ingredient);
   <ElDrawer
     :modelValue="modelValue"
     @update:modelValue="emit('update:modelValue', $event)"
+    :with-header="false"
   >
-    asdf
+    <div class="h-full flex flex-col">
+      <h1 class="font-bold">补充特性</h1>
+      <hr />
+      <el-table
+        :data="ingredient"
+        header-cell-class-name="el-header-cell"
+        class="flex-1"
+      >
+        <el-table-column prop="name" label="食材" />
+        <el-table-column prop="type" label="类型" width="80" />
+        <el-table-column prop="features" label="补充特性" />
+      </el-table>
+    </div>
   </ElDrawer>
 </template>
