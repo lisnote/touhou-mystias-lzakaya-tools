9c5083d7bcf6a7823848145e21defda63d8c9e87
lisnote <1292580280@qq.com>
2023-05-12 03:08:15 +0800

    体验优化

--- src/components/LisTable/LisTable.vue
+++ src/components/LisTable/LisTable.vue
@@ -1,4 +1,7 @@
 <script setup lang="ts">
+import { isArray } from '@/utils/types';
+import { ElTable } from 'element-plus';
+import { ref, onMounted, onBeforeUnmount } from 'vue';
 const { tableProps, tableData } = defineProps<{
   tableProps: {
     label: string;
@@ -10,20 +13,43 @@ const { tableProps, tableData } = defineProps<{
   }[];
   tableData: any[];
 }>();
+
+const tableRef = ref<InstanceType<typeof ElTable>>();
+function clickRow({ key }: KeyboardEvent) {
+  if (key === 'Enter' && document.activeElement instanceof HTMLDivElement) {
+    document.activeElement.click();
+  }
+}
+onMounted(() => {
+  tableRef.value?.$el.addEventListener('keyup', clickRow);
+});
+onBeforeUnmount(() => {
+  tableRef.value?.$el.removeEventListener('keyup', clickRow);
+});
 </script>
 <template>
   <ElTable
+    ref="tableRef"
     :data="tableData"
     :header-cell-class-name="$style.header"
     v-bind="$attrs"
+    highlight-current-row
   >
     <ElTableColumn
-      v-for="col in tableProps"
+      v-for="(col, cIndex) in tableProps"
       :prop="col.prop"
       :label="col.label"
       :width="col.style?.width"
       :minWidth="col.style?.minWidth"
-    />
+    >
+      <template v-slot="{ row }">
+        <div v-bind="cIndex === 0 ? { tabindex: 0 } : {}">
+          {{
+            isArray(row[col.prop]) ? row[col.prop].join(', ') : row[col.prop]
+          }}
+        </div>
+      </template>
+    </ElTableColumn>
   </ElTable>
 </template>
 <style module>
